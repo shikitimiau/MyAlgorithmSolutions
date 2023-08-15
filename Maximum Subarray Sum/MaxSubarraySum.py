b68e6c30a3d6d08122ff0d79952d01f753377bec
shikitimiau <=>
2023-08-15 15:48:04 -0600

    Se logro hacer el problema de Maximum Subarray Sum en tiempo lineal.

--- Maximum Subarray Sum/MaxSubarraySum.py
+++ Maximum Subarray Sum/MaxSubarraySum.py
@@ -22,27 +22,23 @@ def maxsubarraysum(lista: List[int]):
     longitudLista = len(lista)
     listaDePesos = [0] * (longitudLista + 1)
     listaDePesoMinimo = [0] * (longitudLista + 1)
-    
+    indexInicioPesoMinimo = 0
     for i in range(longitudLista):
         listaDePesos[i+1] = lista[i] + listaDePesos[i]
         if listaDePesos[i+1] < listaDePesoMinimo[i]:
             listaDePesoMinimo[i+1] = listaDePesos[i+1]
+            indexInicioPesoMinimo = i+1
         else:
             listaDePesoMinimo[i+1] = listaDePesoMinimo[i]
-
-    for i in range(longitudLista):
         pesoMaxAux = listaDePesos[i+1] - listaDePesoMinimo[i]
         if(pesoMaxAux >= result[1]):
-            for j in range(i+1):
-                if listaDePesoMinimo[j] == listaDePesoMinimo[i]:
-                    result = (lista[j:i + 1], pesoMaxAux)
-                    break
+            result = (lista[indexInicioPesoMinimo:i + 1], pesoMaxAux)
     return result
     
 
 # -------------------------- PRUEBAS --------------------------
 
-#lista = [5,10,(-20), 12, 17, (-10), 5, (-12), 13, (-5), 20]
+# lista = [5,10,(-20), 12, 17, (-10), 5, (-12), 13, (-5), 20]
 # Peso = 40 Intervalo = [12, 17, (-10), 5, (-12), 13, (-5), 20]
 
 # lista = [5, 10, (-20), 12, 30, (-10), 5, (-12), 12, (-5), 4]
@@ -51,7 +47,7 @@ def maxsubarraysum(lista: List[int]):
 # lista = [4, 3, (-10), 3, (-1), 2, 0, (-3), 5, 7, (-4), (-8), (-10), 4, 7, (-30), (-2), (-6), 4, 7]
 # Peso = 13 Intervalo = [3, (-1), 2, 0, (-3), 5, 7]
 
-lista = [5, 5, 5, (-10), (-10), (-10), 7, 7, 7]
+# lista = [5, 5, 5, (-10), (-10), (-10), 7, 7, 7]
 # Peso = 21 Intervalo = [7, 7, 7]
 
 resultado = maxsubarraysum(lista)
